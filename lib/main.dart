eef6c25357f55cba49130103b322c28c439c99b1
ayim-en <ayimen.dev@gmail.com>
2026-04-28 14:48:35 -0700

    feat: created intial create plant profile view

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,22 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-      ChangeNotifierProvider(
-        create: (_) => PlantProvider(),
-        child: const MyApp(),)
-      );
+    ChangeNotifierProvider(
+      create: (_) => PlantProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PlantMe Mobile App',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       home: const MyHomePage(title: 'PlantMe'),
     );
